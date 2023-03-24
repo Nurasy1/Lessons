797493d6cec5e64f2340f7cebf768299489486f3
100ping <princenurik04@mail.ru>
2023-03-24 10:30:44 +0600

    Update diffie-hellmaan.js

--- Cryptography/diffie-hellmaan.js
+++ Cryptography/diffie-hellmaan.js
@@ -1,13 +1,9 @@
 const P = 17;
 const G = 11;
 
-function password(a, b) {
+function double(a, b) {
     let A = G ** (a) % P;
     let B = G ** (b) % P;
-    if (A === 0 && B === 0) {
-        return true;
-    }
-
     let S1 = A ** (b) % P;
     let S2 = B ** (a) % P;
     if (S1 === S2) {
@@ -16,4 +12,25 @@ function password(a, b) {
     return [A, B];
 }
 
-console.log(password(11, 12));
+console.log(double(11, 12));
+/*
+
+const P = 37;
+const G = 23;
+
+function trio(a, b, c) {
+    let A = G ** (a) % P;
+    let B = G ** (b) % P;
+    let C = G ** (c) % P;
+    let S1 = A ** (b * c) % P;
+    let S2 = B ** (a * c) % P;
+    let S3 = C ** (a * b) % P;
+    let key = G ** (a * b * c) % P;
+    if (S1 === S2 && S1 === S3 && S2 === S3) {
+        console.log('Key:');
+    }
+    return key;
+}
+
+console.log(trio(3, 5, 7));
+*/
